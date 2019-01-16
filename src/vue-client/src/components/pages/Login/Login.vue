bd3930519fd5c08522bebe198c979cab83b38463
sharkshen <35325899+sharkshen@users.noreply.github.com>
2019-01-16 12:57:27 +0800

    登录注册表单验证

--- src/vue-client/src/components/pages/Login/Login.vue
+++ src/vue-client/src/components/pages/Login/Login.vue
@@ -1,13 +1,215 @@
 <template>
-<div></div>
+<div class="background">
+  <div class="suibian1">
+    <el-form :model="RegistForm" v-show="this.RegistFlag" status-icon :rules="rules2" ref="RegistForm" label-width="80px" class="demo-ruleForm" >
+      <el-form-item label="用户名" prop="userName" style="color: red">
+        <el-input type="text" v-model="RegistForm.userName" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass" style="color: red">
+    <el-input type="password" v-model="RegistForm.pass" autocomplete="off"></el-input>
+    </el-form-item>
+    <el-form-item label="确认密码" prop="checkPass">
+    <el-input type="password" v-model="RegistForm.checkPass" autocomplete="off"></el-input>
+    </el-form-item>
+    <!--<el-form-item label="年龄" prop="age">-->
+    <!--<el-input v-model.number="RegistForm.age"></el-input>-->
+    <!--</el-form-item>-->
+      <el-form-item>
+        <el-button @click="resetForm('RegistForm')" style="margin-left: -25px ">重置</el-button>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <el-button type="primary" @click="Regist('RegistForm')" size="middle" style="margin-left: -25px ">注册</el-button>
+        <!--<el-button type="primary" @click="Login('RegistForm')">登录</el-button>-->
+      </el-form-item>
+
+    </el-form>
+
+    <el-form :model="LoginForm" v-show="this.LoginFlag" status-icon :rules="rules" ref="LoginForm" label-width="80px" class="demo-ruleForm" >
+      <el-form-item label="用户名" prop="userName" style="color: red">
+        <el-input type="text" v-model="LoginForm.userName" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass" style="color: red">
+        <el-input type="password" v-model="LoginForm.pass" autocomplete="off"></el-input>
+      </el-form-item>
+      <!--<el-form-item label="确认密码" prop="checkPass">-->
+        <!--<el-input type="password" v-model="RegistForm.checkPass" autocomplete="off"></el-input>-->
+      <!--</el-form-item>-->
+      <!--<el-form-item label="年龄" prop="age">-->
+      <!--<el-input v-model.number="RegistForm.age"></el-input>-->
+      <!--</el-form-item>-->
+      <el-form-item>
+        <el-button type="primary" @click="showRegist()" size="middle" style="margin-left: -25px ">注册</el-button>
+        <el-button @click="resetForm('LoginForm')">重置</el-button>
+        <el-button type="primary" @click="Login('LoginForm')">登录</el-button>
+      </el-form-item>
+
+    </el-form>
+
+  </div>
+
+
+</div>
 </template>
 
 <script>
   export default {
-    name: 'Login'
+    name: 'Login',
+    data() {
+      var checkAge = (rule, value, callback) => {
+        if (!value) {
+          return callback(new Error('年龄不能为空'));
+        }
+        setTimeout(() => {
+          if (!Number.isInteger(value)) {
+            callback(new Error('请输入数字值'));
+          } else {
+            if (value < 18) {
+              callback(new Error('必须年满18岁'));
+            } else {
+              callback();
+            }
+          }
+        }, 1000);
+      };
+      var validatePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+          if (this.RegistForm.checkPass !== '') {
+            this.$refs.RegistForm.validateField('checkPass');
+          }
+          callback();
+        }
+      };
+      var validatePass2 = (rule, value, callback) => {
+
+        if (value === '') {
+          callback(new Error('请再次输入密码'));
+        } else if (value !== this.RegistForm.pass) {
+          callback(new Error('两次输入密码不一致!'));
+        } else {
+          callback();
+        }
+      };
+      var validateLoginPass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入密码'));
+        } else {
+
+          callback();
+        }
+      };
+      var checkUserName = (rule, value, callback) => {
+        if (value === '') {
+          return callback(new Error('用户名不能为空'));
+        }else{
+          //TODO
+          callback();
+
+        }
+
+
+      };
+      return {
+        LoginFlag : true,
+        RegistFlag:false,
+        LoginForm: {
+          userName:'',
+          pass:''
+        },
+        RegistForm: {
+          userName:'',
+          pass: '',
+          checkPass: '',
+        },
+        rules: {
+          userName: [
+            { validator: checkUserName, trigger: 'blur' }
+          ],
+          pass: [
+            { validator: validateLoginPass, trigger: 'blur' }
+          ],
+        },
+        rules2: {
+          pass: [
+            { validator: validatePass, trigger: 'blur' }
+          ],
+          checkPass: [
+            { validator: validatePass2, trigger: 'blur' }
+          ],
+          userName: [
+            { validator: checkUserName, trigger: 'blur' }
+          ]
+        }
+      };
+    },
+    methods: {
+      showRegist(){
+        this.RegistFlag=true;
+        this.LoginFlag=false
+      },
+      Login(formName){
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('login!');
+          } else {
+            console.log('error login!!');
+            return false;
+          }
+        });
+      },
+      Regist(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {
+            alert('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName) {
+        this.$refs[formName].resetFields();
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style >
+.background{
+
+  background:url(/static/image/aurora.jpg) ;
+  background-size:cover;
+  height: 100vh;
+  width: 100%;
+}
+  .suibian1{
+    width: 300px;
+    height: 400px;
+    left: 50%;
+    top: 50%;
+    position: absolute;
+    margin-left: -150px;
+    margin-top: -200px;
+    /*background-color: white;*/
+    /*border: 1px solid #000;*/
+    color: red;
+
+  }
+  .demo-ruleForm{
+
+  }
+.el-form-item__label {
+  text-align: right;
+  float: left;
+  font-size: 14px;
+  color: darkorange;
+  line-height: 40px;
+  padding: 0 12px 0 0;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  font-weight: bold;
+}
+
+
 
 </style>
--- src/vue-client/src/router/index.js
+++ src/vue-client/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Login from '../components/pages/Login/Login'
+import Login from '@/components/pages/Login/Login'
 
 Vue.use(Router)
 
